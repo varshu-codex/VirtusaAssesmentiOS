1c9f1bdb2a6d1e8a388702a989b2b44f1cefe4cd
varshini <varshini.v@unilogcorp.com>
2022-05-23 23:00:37 +0530

    Initial Commit

---
+++ VirtusaAssesment/VirtusaAssesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtusaAssesmentApp.swift
+//  VirtusaAssesment
+//
+//  Created by Unilog on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct VirtusaAssesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
